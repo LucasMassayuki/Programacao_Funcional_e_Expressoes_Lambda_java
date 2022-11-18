db568a820c908b0311f237d2a6ec392ff2101b7f
Lucas <lucasofficialcruz95@gmail.com>
2022-11-18 15:51:00 -0300

    Consumer

--- src/application/Program.java
+++ src/application/Program.java
@@ -17,12 +17,10 @@ public class Program {
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
 		
-		double min = 100.0;
+		double factor = 1.1;
+		//.foreach recebe o Consumer como argumento
+		list.forEach(p -> p.setPrice(p.getPrice() * factor));
 		
-		list.removeIf(p -> p.getPrice() >= min);
-		
-		for (Product p : list) {
-			System.out.println(p);
-		}
+		list.forEach(System.out::println);
 	}
 }
