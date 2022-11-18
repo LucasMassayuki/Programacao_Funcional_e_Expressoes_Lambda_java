a4e1156b3f070be5238e0527d601891437edd21c
Lucas <lucasofficialcruz95@gmail.com>
2022-11-18 18:43:42 -0300

    Function

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,24 +3,26 @@ package application;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.stream.Collectors;
 
 import entities.Product;
+import util.UpperCaseName;
 
 public class Program {
 
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		List<Product> list = new ArrayList<>();
-		
+
 		list.add(new Product("Tv", 900.00));
 		list.add(new Product("Mouse", 50.00));
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
+		//convertendo list para stream usando .stream()
+		//.map() vai aplicar uma função a todos elementos de uma stream
+		//convertendo de volta para lista usando .collect(Collectors.toList())
+		List<String> names = list.stream().map(p -> p.getName().toUpperCase()).collect(Collectors.toList());
 		
-		double factor = 1.1;
-		//.foreach recebe o Consumer como argumento
-		list.forEach(p -> p.setPrice(p.getPrice() * factor));
-		
-		list.forEach(System.out::println);
+		names.forEach(System.out::println);
 	}
 }
